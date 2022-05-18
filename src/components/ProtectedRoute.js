dc5d2eca2ddf3d0a3f590d8de21e1d2757033be6
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-05-18 15:04:42 +0200

    feat: create new components

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+// function ProtectedRoute({ components: Components, ...props }) {
+//   console.log(Components)
+//   return (
+//     <Route>
+//       {
+//         () => !props.loggedIn ? <Redirect to="/login" /> : Components.forEach(Component => <Component {...props} />)  
+//       }
+//     </Route>
+//   )
+// }
+
+function ProtectedRoute({ component: Component, ...props }) {
+  return (
+    <Route>
+      {
+        () => !props.loggedIn ? <Redirect to="/login" /> : <Component {...props} />  
+      }
+    </Route>
+  )
+}
+
+export default ProtectedRoute;
