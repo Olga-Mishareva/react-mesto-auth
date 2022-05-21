8f1f7fadc92a4e44af5360065c10a340fc842211
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-05-21 11:14:02 +0200

    fix: sign-in in protected route

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -13,7 +13,7 @@ import { Route, Redirect } from "react-router-dom";
 function ProtectedRoute({ component: Component, ...props }) {
   return (
     <Route>
-      {props.loggedIn ? <Component {...props} /> : <Redirect to="/sing-in" />}
+      {props.loggedIn ? <Component {...props} /> : <Redirect to="/sign-in" />}
     </Route>
   )
 }
