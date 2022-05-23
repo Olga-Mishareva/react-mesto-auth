f89d9af8bc01fbaa259c645c50c5d4eab194990f
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-05-23 13:09:45 +0200

    feat: add mobile menu by logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../images/logo.svg';
+import closeBtn from '../images/close-icon.svg';
+import burgerBtn from '../images/burger-menu.svg';
 
 function Header({ loggedIn, email, resetValidation, onSignOut }) {
+  const [isInvisible, setIsInvisible] = useState(true);
+  const mobileButton = isInvisible ? burgerBtn : closeBtn;
+
+  function handleBurgerButton() {
+    isInvisible ? setIsInvisible(false): setIsInvisible(true);
+  }
+
   return (
     <header className="header">
       <div className="header__container">
-        <img className="logo" src={logo} alt="Логотип"/>
-        <div className="header__auth-container">
+        <div className="header__logo-container">
+          <img className="logo" src={logo} alt="Логотип"/>
+          <button className="header__burger-btn" type="button" 
+            style={{ backgroundImage: `url(${mobileButton})`}}
+            onMouseDown={handleBurgerButton}></button>
+        </div>
+        <div className={`header__auth-container header__auth-container${isInvisible ? '_invisible' : ''}`}>
           <p className="header__email">{email}</p>
           <button onMouseDown={onSignOut} 
             className={`header__logout header__logout${loggedIn ? '_active' : ''}`}>
